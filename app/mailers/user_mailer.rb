5b99fc7fbc0df4be9af1b9c9cc9fcd9fd8947a25
Peter <plowry812@gmail.com>
2013-06-12 14:58:00 -0700

    heroku as host

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    @url  = "http://coachatlas.com/users/sign_in"
+    @url  = "http://coachatlasv3.herokuapp.com/users/sign_in"
     mail(:to => user.email, :subject => "Welcome to CoachAtlas")
   end
 end
